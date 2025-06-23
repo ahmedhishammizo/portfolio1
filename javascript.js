7164931f7594341a661caf1b7683efa220cf46aa
ahmed0115 <110282159+ahmedhishammizo@users.noreply.github.com>
2025-06-23 20:52:25 +0300

    Delete javascript.js

--- javascript.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("menu-toggle")?.addEventListener("click", () =>
-      document.getElementById("nav-links")?.classList.toggle("show")
-    );
-  });
-  
