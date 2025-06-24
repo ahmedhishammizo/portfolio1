ad773ffd49d8db86edb05d2a06b3418d647003b0
ahmed0115 <110282159+ahmedhishammizo@users.noreply.github.com>
2025-06-24 14:45:42 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("menu-toggle")?.addEventListener("click", () =>
+      document.getElementById("nav-links")?.classList.toggle("show")
+    );
+  });
+  
